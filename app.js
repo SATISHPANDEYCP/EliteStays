3428166addc5fd8e300b15febd1fb64f5bf04641
Your Name <satishpandeycp@gmail.com>
2025-02-06 14:47:04 +0530

    some changes

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/EliteStays";
 
 main()
   .then(() => {
-    console.log("connected to DB");
+    console.log(`Connected to MongoDB: ${MONGO_URL.includes("mongodb+srv") ? "Online (Atlas)" : "Offline (Local)"}`);
   })
   .catch((err) => {
     console.log(err);
